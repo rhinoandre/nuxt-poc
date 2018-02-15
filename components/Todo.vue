08e985cafb2801e629015bc42ba4665bb6d01019
Rhino <andre.bazoli@watchguard.com>
2018-02-15 18:49:54 -0200

    Implementing Todo component

--- components/Todo.vue
+++ components/Todo.vue
@@ -1,12 +1,26 @@
 <template>
   <li>
-    <input type="checkbox" :id="todo.id" v-model="todo.done">
+    <input type="checkbox" :id="todo.id" v-model="todo.done" @click="inputClick()">
     <label for="list3" :class="{ done: todo.done }">{{ todo.label }}</label>
   </li>
 </template>
 
 <script>
 export default {
-  props: ['todo']
+  props: ['todo'],
+  created () {
+    console.log(this.todo)
+  },
+  methods: {
+    inputClick() {
+      this.todo.value = 'tes'
+    }
+  }
 }
 </script>
+
+<style>
+.done {
+    text-decoration-line: line-through;
+}
+</style>
